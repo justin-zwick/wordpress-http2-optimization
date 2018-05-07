ab8ed7641e7e3eb84122de050ffc36a3c37f0b5c
o10n-x <info@optimization.team>
2018-05-07 21:19:15 +0200

    core update

--- admin/http2-intro.inc.php
+++ admin/http2-intro.inc.php
@@ -28,7 +28,7 @@ $module_version = $view->module->version();
 			
 			<p class="about-text" style="min-height:inherit;">This plugin is a toolkit for advanced HTTP/2 optimization for WordPress.</p>
 
-			<p class="about-text info_white" style="min-height:inherit;border-color:#0073aa;background:#f1faff;"><strong><span class="dashicons dashicons-welcome-comments" style="line-height: 32px;font-size: 34px;width: inherit;color:#0073aa;"></span></strong> The optimization plugins have been removed from WordPress.org. Read the story <a href="https://github.com/o10n-x/wordpress-css-optimization/issues/4" target="_blank">here</a>.</p>
+			<!--p class="about-text info_white" style="min-height:inherit;border-color:#0073aa;background:#f1faff;"><strong><span class="dashicons dashicons-welcome-comments" style="line-height: 32px;font-size: 34px;width: inherit;color:#0073aa;"></span></strong> The optimization plugins have been removed from WordPress.org. Read the story <a href="https://github.com/o10n-x/wordpress-css-optimization/issues/4" target="_blank">here</a>.</p-->
 
 			<p class="about-text" style="min-height:inherit;">Getting started? Read <a href="https://developers.google.com/web/fundamentals/performance/http2/" target="_blank">this article</a> about HTTP/2 Optimization by Google. Test your server for HTTP/2 support <a href="https://tools.keycdn.com/http2-test?url=<?php print urlencode(home_url()); ?>" target="_blank" rel="noopener">here</a>.</p>
 
